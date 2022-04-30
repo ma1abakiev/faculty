e71e3e521d26b4bd280be747bcc0107a54c39c9b
ma1abakiev <malabakievramzan@gmail.com>
2022-04-30 23:59:30 +0600

    30.04.2022 23:59

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 $(function() {
 
 
-    $('.item').mouseover(function() {
-        $(this).addClass('item_active')
+    // $('.item').mouseover(function() {
+    //     $(this).addClass('item_active')
         
+    // })
+    // $('.item').mouseout(function() {
+    //     $(this).removeClass('item_active')
+    // })    
+
+    $('.item').hover(function() {
+        $(this).toggleClass('item_active')
     })
-    $('.item').mouseout(function() {
-        $(this).removeClass('item_active')
-    })    
 
 })
